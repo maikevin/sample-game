2c296fd963c4c817f5731fe045c7a6ad5e73f5dc
Kevin Mai <maikevin@users.noreply.github.com>
2024-11-26 14:50:31 -0800

    This is my commit for November 26, 2024

---
+++ sample.py
@@ -0,0 +1,27 @@
+
+      if computer_input == 6:
+        print("1:thigh")
+        print("2:forearm")
+        print("3:metal")
+        if user_input == "1":
+          print('nope! try again')
+          section2()
+        if user_input == "2":
+          print("yep! you're still wrong though")
+          section2()
+        if user_input == "3":
+          print('correct! now pick a veggie')
+
+      if computer_input == 7:
+        print('stop making a sandwich! things arent right!')
+        print("1:stop talking")
+        print("2:SHUt UP")
+        print("3:i know")
+        if user_input == "1":
+          print('you cant make me, im you')
+          section2()
+        if user_input == "2":
+          print("no")
+          section2()
+        if user_input == "3":
+          print('then stop and listen')
